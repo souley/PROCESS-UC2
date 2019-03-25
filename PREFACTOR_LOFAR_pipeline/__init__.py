d3138ce235b8981bc38c00eaa3367e5388c2bec7
souley <s.madougou@esciencecenter.nl>
2019-03-25 10:24:30 +0100

    Adding xenon-flow-specific  properties

--- PREFACTOR_LOFAR_pipeline/__init__.py
+++ PREFACTOR_LOFAR_pipeline/__init__.py
@@ -51,5 +51,6 @@ def run_pipeline(observation, **kargs):
         "workflow": "workflow.cwl",
         "input": {}
     }
-    res = requests.post(url, headers=headers, data=json.dumps(data))
-    return res.status_code
+#    res = requests.post(url, headers=headers, data=json.dumps(data))
+#    return res.status_code
+    return "Testing ..."
